cf4f10f758043e5438c330d30ca855be6b93297c
Souvik Kundu <kivtech88@gmail.com>
2024-01-14 18:30:16 +0530

    commit

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,6 @@
 'use strict';
+//console.log(document.querySelector('.message').textContent);
+console.log(document.querySelector('.message').textContent = '🍕 Correct Number!');
+document.querySelector('.score').textContent = 10;
+document.querySelector('.number').textContent = 13;
+document.querySelector('.guess').value = 23;

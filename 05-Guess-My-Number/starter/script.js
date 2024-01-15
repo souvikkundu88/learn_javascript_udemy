13a3c040e210bb28f9171afdc64b4e50a3225632
Souvik Kundu <kivtech88@gmail.com>
2024-01-15 21:53:09 +0530

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -9,33 +9,33 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 //console.log(number);
 document.querySelector('.number').textContent = secretNumber;
-document.querySelector('.check').addEventListener('click', function() {
-    const guess = Number(document.querySelector('.guess').value);
-    console.log(guess, typeof guess);
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
 
-    if(!guess) {
-        document.querySelector('.message').textContent = '⛔ No number!'
-    } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = '🍕 Correct number';
-    } else if (guess > secretNumber) {
+  if (!guess) { // when guess has 0 or no number input
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) { // when guess number matches with random number
+    document.querySelector('.message').textContent = '🍕 Correct number';
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.number').style.width = '30rem';
+  } else if (guess > secretNumber) { // when guess is high
         if (score > 1) {
-            score--;
-            document.querySelector('.score').textContent = score;
-            document.querySelector('.message').textContent = 'higher';
+        score--;
+        document.querySelector('.score').textContent = score;
+        document.querySelector('.message').textContent = 'higher';
         } else {
-            document.querySelector('.message').textContent = 'you lost!';
-            document.querySelector('.score').textContent = 0;
+        document.querySelector('.message').textContent = 'you lost!';
+        document.querySelector('.score').textContent = 0;
         }
-    }
-    else if (guess < secretNumber) {
-        if(score > 1) {
-            score--;
-            document.querySelector('.score').textContent = score;
-            document.querySelector('.message').textContent = 'lower';
+   } else if (guess < secretNumber) { // when guess is low
+        if (score > 1) {
+        score--;
+        document.querySelector('.score').textContent = score;
+        document.querySelector('.message').textContent = 'lower';
         } else {
-            document.querySelector('.message').textContent = 'you lost!';
-            document.querySelector('.score').textContent = 0;
+        document.querySelector('.message').textContent = 'you lost!';
+        document.querySelector('.score').textContent = 0;
         }
-    }
+   }
 });
-

d56b7b00575f3a4cd465746bffec5a0b83883610
Souvik Kundu <kivtech88@gmail.com>
2024-01-15 21:05:55 +0530

    Update script.js

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,12 +5,22 @@
 // document.querySelector('.number').textContent = 13;
 // document.querySelector('.guess').value = 23;
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+//console.log(number);
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
     if(!guess) {
         document.querySelector('.message').textContent = '⛔ No number!'
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🍕 Correct number';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'higher';
+    }
+    else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = 'lower';
     }
 });
 

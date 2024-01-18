a9d2e234d195c282b1e98fe463cd6e0c90965c45
Souvik Kundu <kivtech88@gmail.com>
2024-01-18 21:48:05 +0530

    commit

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,8 +4,10 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
-const btnNew = documnet.querySelector('.btn--new');
-const btnRoll = documnet.querySelector('.btn--roll');
+
+
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // starting conditions
@@ -13,13 +15,16 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+
+
 // rolling dice functionality
 btnRoll.addEventListener('click', function() {
     // 1. generating random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
 
     // 2. display dice
-
+    diceEl.classList.remove('hidden');
+    dice.src = `dice-${dice}.png`;
 
     // 3. check for a rolled 1: if true , switch to next player
 

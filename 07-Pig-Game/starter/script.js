25af9679d7139b11a49279dac12d9b842cfc1e0e
Souvik Kundu <kivtech88@gmail.com>
2024-01-19 03:24:24 +0530

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -5,7 +5,6 @@ const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
 
-
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -15,17 +14,25 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-
+let currentScore = 0;
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function() {
     // 1. generating random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
+    console.log(dice);
 
     // 2. display dice
     diceEl.classList.remove('hidden');
-    dice.src = `dice-${dice}.png`;
+    diceEl.src = `dice-${dice}.png`;
 
     // 3. check for a rolled 1: if true , switch to next player
+    if(dice !== 1) {
+        // add dice to the current score
+        currentScore += score0El;
 
+    } else {
+        // switch to the next player
+        currentScore = 0;
+    }
 })

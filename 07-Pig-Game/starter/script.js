bd8e37a7cc82f93abb68d17c8530387e5fdd8402
Souvik Kundu <kivtech88@gmail.com>
2024-01-19 06:05:06 +0530

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -14,14 +14,36 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+// score0El.textContent = 0;
+// score1El.textContent = 0;
+// diceEl.classList.add('hidden');
+// const score = [0, 0];
+// let currentScore = 0;
+// let activePlayer = 0;
+// let playing = true;
 
-const score = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let score, currentScore, activePlayer, playing;
+
+const init = function() {
+    score = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    diceEl.classList.add('hidden');
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    
+    player0EL.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0EL.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    activePlayer = 0;
+    playing = true;
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -32,19 +54,10 @@ const switchPlayer = function () {
 };
 
 // reset game, new game
-btnNew.addEventListener('click', function() {
-    score0El.textContent = 0;
-    score1El.textContent = 0;
-    diceEl.classList.add('hidden');
-    current0El.textContent = 0;
-    current1El.textContent = 0;
-    activePlayer = 0;
-    playing = true;
-    player0EL.classList.remove('player--winner');
-    player1El.classList.remove('player--winner');
-    player0EL.classList.add('player--active');
-    player1El.classList.remove('player--active');
-});
+// btnNew.addEventListener('click', function() {
+    
+
+// });
 
 
 // rolling dice functionality

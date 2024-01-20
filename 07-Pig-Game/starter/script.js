0b1af31c669b9c923f52b6df6fcd5bb5077978f9
Souvik Kundu <kivtech88@gmail.com>
2024-01-20 23:22:25 +0530

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -58,7 +58,7 @@ btnNew.addEventListener('click', init);
 
 
 // rolling dice functionality
-btnRoll.addEventListener('click', function() {
+('click', function() {
   if(playing) {
     // 1. generating random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;

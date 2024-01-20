4d84c7e36eb42354fc13eb3c7b73bdb5fb748cfc
Souvik Kundu <kivtech88@gmail.com>
2024-01-20 23:20:20 +0530

    Update script.js

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -66,7 +66,7 @@ btnRoll.addEventListener('click', function() {
 
     // 2. display dice
     diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`;
+     = `dice-${dice}.png`;
 
     // 3. check for a rolled 1: if true , switch to next player
     if (dice !== 1) {
